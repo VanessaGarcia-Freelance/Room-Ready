685c326513bc5d98b56fe1ed62a21944fe8747f8
Vanessa Garcia <me@vanessa-lyn.com>
2017-06-02 00:51:44 -0700

    reorganizing files to have a directory of all the pages.

--- Gruntfile.js
+++ Gruntfile.js
@@ -87,7 +87,7 @@ module.exports = function(grunt) {
         },
         files: [ {
           cwd: "src/templates/",
-          src: "**/*.pug",
+          src: "*.pug",
           dest: "dist/",
           expand: true,
           ext: ".html"
